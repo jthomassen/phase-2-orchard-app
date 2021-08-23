66eed195cb4cb40fc4d7881c3c733529225febf4
Jeff Thomassen <jeff.thomassen@ucdenver.edu>
2021-08-23 12:50:48 -0600

    draft app

---
+++ orchard-app/src/components/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import Header from './Header'
+import NavBar from './NavBar'
+import PostContainer from './PostContainer'
+import Map from './Map'
+
+function App() {
+    <div>
+        App
+        <Header />
+        <NavBar />
+        <PostContainer />
+        <Map />
+    </div>
+}
+
+export default App
---
+++ orchard-app/src/components/NavBar.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+function NavBar() {
+    <div>
+        Nav Bar
+    </div>
+}
+
+export default NavBar
